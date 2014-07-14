18d9cd3dd89dcb199aede08dddfa76102ba17d80
Adam Parkin <aparkin@seekersolutions.com>
2014-07-13 22:56:41 -0700

    Added base unit test

--- src/test/java/com/pzelnip/TestDefault.java
+++ src/test/java/com/pzelnip/TestDefault.java
@@ -1,3 +1,14 @@
+package com.pzelnip;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestDefault {
+	
+	@Test
+	public void foobar() {
+		assertTrue(true);
+		assertFalse(false);
+		assertEquals("foobar", "foobar");
+	}
 }
